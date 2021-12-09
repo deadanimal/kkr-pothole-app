b9b47478e6418b758e6f55adb82cb3be2099e3e9
Dinie Afeeq <dinieafiq@pipeline.com.my>
2021-12-09 15:40:09 +0800

    .

--- src/app/core/user/profile/profile.page.ts
+++ src/app/core/user/profile/profile.page.ts
@@ -164,14 +164,9 @@ export class ProfilePage implements OnInit {
         image: new FormControl(null),
         gambar_id: new FormControl(null),
         password: new FormControl(null, [
-<<<<<<< HEAD
-          Validators.pattern('[a-zA-Z0-9_+-@$!%*?&:]*'),
-          Validators.minLength(8),
-=======
           // Validators.pattern('[a-zA-Z0-9_.+-]*'),
           Validators.minLength(8),
           // Validators.required
->>>>>>> dd4ab09cc5cab78f5103aa7f930297a1dd98b791
         ]),
         confirmpassword: new FormControl(null),
       },
